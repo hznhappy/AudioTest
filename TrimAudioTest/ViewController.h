ed2882570edde2f21ee0d07d9cb1ee0edb1e60ca
andy <hznhappy@163.com>
2012-04-13 16:53:17 +0800

    Initial Commit

---
+++ TrimAudioTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TrimAudioTest
+//
+//  Created by apple on 4/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
